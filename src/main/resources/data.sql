5183599a4b53e016b22ad16d0d9f4f24e699c87d
Alexandre Krispin <alexandre.krispin@upstairs.co.jp>
2019-08-10 11:04:11 +0900

    Update test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,13 +11,15 @@ INSERT INTO plan (label, price) VALUES ('Basic', 03.99);
 INSERT INTO plan (label, price) VALUES ('Standard', 10.99);
 INSERT INTO plan (label, price) VALUES ('Premium', 15.99);
 
-INSERT INTO payment (card_issuer, cardholder_name, card_number, cvv, expiration_date, address, city, postal_code, state_province, country)
-VALUES ('MasterCard', 'John Doe', '1111-1111-1111-1111', 111, PARSEDATETIME('2018/11/01', 'yyyy/mm/dd'), '1st street of Washington', 'Seattle', '98105', 'Washington', 'USA');
-INSERT INTO payment (card_issuer, cardholder_name, card_number, cvv, expiration_date, address, city, postal_code, state_province, country)
-VALUES ('Visa', 'Jane Doe', '2222-2222-2222-2222', 222, PARSEDATETIME('2010/12/01', 'yyyy/mm/dd'), 'Shimotakaido 8-21-5', 'Tokyo', 'Suginami', '112-2103', 'Tokyo');
+INSERT INTO payment_method (card_issuer, cardholder_name, card_number, cvv, expiration_date, address, city, postal_code, state_province, country, payer_email)
+VALUES ('MasterCard', 'John Doe', '1111-1111-1111-1111', 111, PARSEDATETIME('2018/11/01', 'yyyy/mm/dd'), '1st street of Washington', 'Seattle', '98105', 'Washington', 'USA', 'john.doe@gmail.com');
+INSERT INTO payment_method (card_issuer, cardholder_name, card_number, cvv, expiration_date, address, city, postal_code, state_province, country, payer_email)
+VALUES ('Visa', 'Jane Doe', '2222-2222-2222-2222', 222, PARSEDATETIME('2010/12/01', 'yyyy/mm/dd'), 'Shimotakaido 8-21-5', 'Tokyo', 'Suginami', '112-2103', 'Tokyo', 'jane.doe@gmail.com');
 
-INSERT INTO user (email, first_name, last_name, password, is_staff, profile_picture) VALUES ('sarah.connor@gmail.com', 'Sarah', 'Connor', 'nope', TRUE, 'http://localhost:8080/profile_picture.png');
-INSERT INTO user (email, first_name, last_name, password, is_staff, profile_picture) VALUES ('david.bowie@gmail.com', 'David', 'Bowie', 'yeah', TRUE, 'http://localhost:8080/profile_picture.png');
+INSERT INTO user (email, first_name, last_name, password, is_staff, profile_picture)
+VALUES ('sarah.connor@gmail.com', 'Sarah', 'Connor', 'nope', TRUE, 'http://localhost:8080/profile_picture.png');
+INSERT INTO user (email, first_name, last_name, password, is_staff, profile_picture)
+VALUES ('david.bowie@gmail.com', 'David', 'Bowie', 'yeah', TRUE, 'http://localhost:8080/profile_picture.png');
 
 INSERT INTO user (email, first_name, last_name, password, phone, profession_label, membership_label, payment_method_id, profile_picture)
 VALUES ('john.doe@gmail.com', 'John', 'Doe', 'toto', '080-1111-2222', 'Accountant', 'Basic', 1, 'http://localhost:8080/profile_picture.png');
@@ -38,5 +40,5 @@ INSERT INTO message (author_email, content, ticket_id) VALUES ('david.bowie@gmai
 INSERT INTO message (author_email, content, ticket_id) VALUES ('jane.doe@gmail.com', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 2);
 INSERT INTO message (author_email, content, ticket_id) VALUES ('jane.doe@gmail.com', 'Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque.', 2);
 
-INSERT INTO transaction (id, delivery_estimate, transactor_email) VALUES ('RF120941', '2018-12-05', 'john.doe@gmail.com');
-INSERT INTO transaction (id, delivery_estimate, transactor_email) VALUES ('RF151093', '2018-12-15', 'jane.doe@gmail.com');
+INSERT INTO order (id, delivery_estimate, transactor_email, plan_label) VALUES ('RF120941', '2018-12-05', 'john.doe@gmail.com', 'Basic');
+INSERT INTO order (id, delivery_estimate, transactor_email, plan_label) VALUES ('RF151093', '2018-12-15', 'jane.doe@gmail.com', 'Standard');
