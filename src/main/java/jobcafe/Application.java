4891e4a8783156a0645395f207544f2fe8fd38de
Alexandre Krispin <alexandre.krispin@upstairs.co.jp>
2019-09-19 09:54:56 +0900

    Add swagger doc

--- src/main/java/jobcafe/Application.java
+++ src/main/java/jobcafe/Application.java
@@ -4,16 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @EnableAutoConfiguration
 @EnableJpaRepositories
 @EnableJpaAuditing
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
+
+	@Bean
+	public Docket docker() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
